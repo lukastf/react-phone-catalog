dc2f38c00034bab9bc5067fe006640fd90c3e6ff
Lucas Trevizan de Freitas <lukas_tf@hotmail.com>
2025-11-15 18:21:00 -0300

    inicio do navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,15 @@
-import logoMobile from "./assets/logo-mobile.svg";
-import logoDesktop from "./assets/logo-desktop.svg";
-//import menuHamburguerMobile from "./assets/menu-hamburguer-mobile.svg";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <header className="fixed z-100 top-0 right-0 left-0 flex gap-4 xl:gap-6 justify-between bg-white">
-        <div
-          className="flex grow gap-4 justify-between h-12 relative md:grow-0 lg:h-16 after:content-[''] 
-          after:block after:h-px after:w-screen after:bg-[#e2e6e9] after:absolute after:bottom-0"
-        >
-          <a href="#" className="flex px-4 lg:px-6">
-            <picture className="flex items-center">
-              <img src={logoMobile} alt="logo" className="block lg:hidden" />
-              <img src={logoDesktop} alt="logo" className="hidden lg:block" />
-            </picture>
-          </a>
-          <div className="relative px-4 flex items-center border-l-[#E2E6E9] border-l md:hidden">
-            <button
-              className="block w-4 h-4 bg-center bg-no-repeat cursor-pointer 
-            bg-[url(./assets/menu-hamburguer-mobile.svg)]"
-            ></button>
-          </div>
-        </div>
-        <nav className="hidden grow md:flex justify-between">
-          <ul className="mt-6 list-none flex flex-col gap-4 md:flex-row md:mt-0 md:gap-8">
-            <li className="flex items-center justify-center">
-              <a
-                aria-current="page"
-                href="#/"
-                className="
-                text-[#313237]
-                uppercase 
-                text-xs 
-                font-extrabold 
-                leading-[22px] 
-                tracking-[2px] 
-                h-full 
-                relative 
-                transition-all 
-                duration-300
-
-                md:flex 
-                md:items-center
-
-                after:content-[''] 
-                after:block 
-                after:h-0.5 
-                after:w-full 
-                after:bg-[#313237] 
-                after:absolute
-                after:bottom-0 
-                after:transition-all 
-                after:duration-300"
-              >
-                Home
-              </a>
-            </li>
-            <li className="flex items-center justify-center">
-              <a
-                aria-current="page"
-                href="#/phones"
-                className="
-                text-[#89939a]
-                hover:text-[#313237]
-                uppercase 
-                text-xs 
-                font-extrabold 
-                leading-[22px] 
-                tracking-[2px] 
-                h-full 
-                relative 
-                transition-all 
-                duration-300
-
-                md:flex 
-                md:items-center
-
-                after:content-[''] 
-                after:block 
-                after:h-0.5 
-                after:w-0 
-                hover:after:w-full
-                after:bg-[#313237] 
-                after:absolute
-                after:bottom-0 
-                after:transition-all 
-                after:duration-300"
-              >
-                Phones
-              </a>
-            </li>
-          </ul>
-          <ul className="flex"></ul>
-        </nav>
-      </header>
-    </>
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <main className="p-8">
+        {/* Outlet renderiza o componente da rota aninhada (Home, About, Contact) */}
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./index.css"; // Seu arquivo CSS principal do Tailwind
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
