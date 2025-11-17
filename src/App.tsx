b4fb6d1d242e0feb2674a416ebae6dec61374743
Lucas Trevizan de Freitas <lukas_tf@hotmail.com>
2025-11-17 08:47:19 -0300

    preparando pra receber o carrosel

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <main className="p-8">
-        {/* Outlet renderiza o componente da rota aninhada (Home, About, Contact) */}
-        <Outlet />
+      <main className="box-border pt-12 lg:pt-16">
+        <div className="max-w-7xl my-0 mx-auto px-4 md:px-6 lg:px-8">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <section className="mb-14 columns-4 grid grid-cols-24 gap-x-5 md:gap-x-6 md:mb-16 md:columns-12 lg:mb-22 lg:columns-24">
+        <h1 className="hidden">Product Catalog</h1>
+        <h2 className="my-6 font-semibold md:text-5xl col-span-full">
+          Welcome to Nice Gadgets store!
+        </h2>
+        <div className="col-span-full grid grid-cols-24 columns-4 gap-x-5 md:gap-x-6 md:columns-12 lg:columns-24">
+          carrosel
+        </div>
+      </section>
     </>
   );
 }
